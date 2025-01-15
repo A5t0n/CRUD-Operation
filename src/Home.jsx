925ee2d1485fadfaba8a54ef992afcc093fcdf16
Aston Varghese <astonvarghese8@gmail.com>
2025-01-15 14:02:13 -0500

    dependencies fix

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import axios from 'axios'
 
 function Home (){
+    useEffect(()=>{
+        axios.get('http://localhost:8081/')
+    },[])
     return (
-        <div>Home</div>
+        <></>
     );
 }
 
