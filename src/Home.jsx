fe7489ac9957af84dd7724c82b0acb5af8d226cb
Aston Varghese <astonvarghese8@gmail.com>
2025-01-16 13:57:07 -0500

    useeffect in home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,8 @@ import axios from 'axios'
 function Home (){
     useEffect(()=>{
         axios.get('http://localhost:8081/')
+        .then(res=> console.log(res))
+        .catch(err=> console.log(err))
     },[])
     return (
         <></>
